d4783f650e0c56329c2f312d05759e223964cd69
vinnycrm <vinnycrm@gmail.com>
2018-04-13 10:58:37 +0530

    updating to t3c docker hub

--- docker_push.sh
+++ docker_push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker build -t vinnycrm/anchel-core:1.0 .
+docker build -t t3ctechnologies/anchel-core:1.0 .
 docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD";
-docker push vinnycrm/anchel-core:1.0
+docker push t3ctechnologies/anchel-core:1.0
