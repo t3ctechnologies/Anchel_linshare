5916e3bf9b0122637b9d433c5206cdc3bed4d785
basusgs <basu@shrigowri.com>
2018-04-20 15:27:48 +0530

    download path updated

--- src/main/java/org/linagora/linshare/core/dao/impl/JcloudObjectStorageFileDataStoreImpl.java
+++ src/main/java/org/linagora/linshare/core/dao/impl/JcloudObjectStorageFileDataStoreImpl.java
@@ -218,18 +218,8 @@ public class JcloudObjectStorageFileDataStoreImpl implements FileDataStore {
 			String gateway_file = gateway_path.concat(new File(fileName).getName());
 			boolean secondMethodStatus = false;
 			FtpClient.init(configArray);
-			for (int i = 10; i < 1000; i++) {
-				if (new File(gateway_file).exists()) {
-					secondMethodStatus = true;
-					break;
-				} else {
-					Thread.sleep(i * 1000);
-				}
-			}
-			if (secondMethodStatus) {
-				FtpClient.init(configArray);
-			}
-			System.err.println("********************FILE DOWNLOADING IS COMPLETED*******************");
+			Thread.sleep(10 * 1000);
+			FtpClient.init(configArray);
 			// TODO Deleting GatewayFile
 			new File(gateway_file).delete();
 		} catch (IOException | InterruptedException e) {
